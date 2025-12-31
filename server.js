63ac033d8cc2b1bd4b692df07cb97dba0196c20a
zainab <zainabazeem324@gmail.com>
2025-12-31 15:12:06 +0500

    cross origin policy

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
+
 app.use(express.json()); // to read JSON body
 
 app.get("/", (req, res) => {
