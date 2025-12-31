634c74d495cdd97cb5f58397f6ed7c9b21d09439
zainab <zainabazeem324@gmail.com>
2025-12-31 15:20:35 +0500

    frontend url

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ import cors from "cors";
 import { transporter } from "./mailer.js";
 
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: FRONTEND_URL }));
 
 app.use(express.json()); // to read JSON body
 
